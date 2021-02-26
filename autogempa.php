a0338204b5f1915cb987bd066427e1e4ce77e8d0
Raksaka Indra <raksakaindra@gmail.com>
2021-02-26 23:53:48 +0700

    Create autogempa.php

---
+++ autogempa.php
@@ -0,0 +1,15 @@
+<?php
+  // Kode Baris PHP untuk Mengolah Data autogempa.xml
+  $data = simplexml_load_file("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.xml") or die("Gagal mengakses!");
+  echo "<h2>Gempabumi Terbaru</h2>";
+  echo "Tanggal: " . $data->gempa->Tanggal . "<br>";
+  echo "Jam: " .  $data->gempa->Jam . "<br>";
+  echo "Magnitudo: " . $data->gempa->Magnitude . "<br>";
+  echo "Kedalaman: " . $data->gempa->Kedalaman . "<br>";
+  echo "Koordinat: " . $data->gempa->point->coordinates . "<br>";
+  echo "Lintang: " . $data->gempa->Lintang . "<br>";
+  echo "Bujur: " . $data->gempa->Bujur . "<br>";
+  echo "Lokasi: " . $data->gempa->Wilayah . "<br>";
+  echo "Potensi: " . $data->gempa->Potensi . "<br>";
+  echo "Shakemap: <br><img src=\"https://data.bmkg.go.id/DataMKG/TEWS/" . $data->gempa->Shakemap . "\" alt=\"Gempabumi Terbaru\">";
+?>
