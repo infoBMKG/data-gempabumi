b5c8cc8fcf3c1cf774b3a2e866912276334137dc
Raksaka Indra <raksakaindra@gmail.com>
2021-02-26 23:55:24 +0700

    Create gempadirasakan.php

---
+++ gempadirasakan.php
@@ -0,0 +1,19 @@
+<?php
+  // Kode Baris PHP untuk Mengolah Data gempadirasakan.xml
+  $data = simplexml_load_file("https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.xml") or die ("Gagal ambil!");
+  echo "<h2>Daftar 15 Gempabumi Dirasakan</h2>";
+  $i = 1;
+  foreach($data->gempa as $gempaDirasakan) {
+    echo "No: " . $i . "<br>";
+    echo "Tanggal: " . $gempaDirasakan->Tanggal . "<br>";
+    echo "Jam: " .  $gempaDirasakan->Jam . "<br>";
+    echo "Magnitudo: " . $gempaDirasakan->Magnitude . "<br>";
+    echo "Kedalaman: " . $gempaDirasakan->Kedalaman . "<br>";
+    echo "Koordinat: " . $gempaDirasakan->point->coordinates . "<br>";
+    echo "Lintang: " . $gempaDirasakan->Lintang . "<br>";
+    echo "Bujur: " . $gempaDirasakan->Bujur . "<br>";
+    echo "Lokasi: " . $gempaDirasakan->Wilayah . "<br>";
+    echo "Dirasakan di Wilayah: " . $gempaDirasakan->Dirasakan . "<br><br>";
+    $i++;
+  }
+?>
