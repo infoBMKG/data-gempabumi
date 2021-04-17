12ab76a61f7a94eb5936e2e9f9104674b2b5e7b5
Raksaka Indra <raksakaindra@gmail.com>
2021-04-17 17:00:52 +0700

    Penambahan key DateTime

--- autogempa.php
+++ autogempa.php
@@ -4,6 +4,7 @@
   echo "<h2>Gempabumi Terbaru</h2>";
   echo "Tanggal: " . $data->gempa->Tanggal . "<br>";
   echo "Jam: " .  $data->gempa->Jam . "<br>";
+  echo "DateTime: " $data->gempa->DateTime . "<br>";
   echo "Magnitudo: " . $data->gempa->Magnitude . "<br>";
   echo "Kedalaman: " . $data->gempa->Kedalaman . "<br>";
   echo "Koordinat: " . $data->gempa->point->coordinates . "<br>";
